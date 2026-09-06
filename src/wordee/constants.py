1e229aee04145b685cb6c917bf5773094098447f
nerrader <nerrader@proton.me>
2026-09-06 11:09:36 +0700

    chore: update name from wordle and wordle-gui to wordee in paths and useragent

--- src/wordee/constants.py
+++ src/wordee/constants.py
@@ -3,14 +3,14 @@ from typing import Literal
 
 from platformdirs import PlatformDirs
 
-_dirs = PlatformDirs("wordle", appauthor="nerrader")
+_dirs = PlatformDirs("wordee", appauthor="nerrader")
 MAIN_DATA_PATH: Path = _dirs.user_data_path
 STATE_PATH: Path = MAIN_DATA_PATH / "state.json"
 LOG_FILE_PATH: Path = MAIN_DATA_PATH / "app.log"
 CACHE_DIR_PATH: Path = MAIN_DATA_PATH / "cache"
 SOLUTIONS_CACHE_PATH = CACHE_DIR_PATH / "possible_solutions.txt"
 
-USER_AGENT = "WordleGUI (https://github.com/nerrader/wordle-gui)"
+USER_AGENT = "WordleGUI (https://github.com/nerrader/wordee)"
 
 GameMode = Literal["daily", "unlimited"]
 WordeeCellColor = Literal["gray", "yellow", "green"]
