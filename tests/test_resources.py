82622f8308db86bb3e55e5a0fb45c13c0b3186e2
nerrader <nerrader@proton.me>
2026-09-08 17:18:35 +0700

    chore: made the resources test get skipped in CI

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,10 +1,17 @@
+import os
 import subprocess
 from pathlib import Path
+
+import pytest
 
 QRC = Path("src/wordee/assets/resources.qrc")
 RESOURCES = Path("src/wordee/assets/resources_rc.py")
 
 
+@pytest.mark.skipif(
+    os.getenv("CI") == "true",
+    reason="Generated RC files differ between local and CI due to random stuff idk",
+)
 def test_resources_rc_is_up_to_date(tmp_path: Path) -> None:
     original_rc_file = RESOURCES.read_bytes()
 
