b5cb00082cf69a56e680157b0e5007819e12ea95
nerrader <nerrader@proton.me>
2026-09-09 17:00:12 +0700

    revert: commit 2490155 (remove --create-linux-installer and im gonna use .bin instead)
    i reverted this because a nuitka maintainer reached out to me saying, hey you should use app-dist instead and maybe itll solve it so im gonna do that before actually scrapping the idea of using .AppImage
    
    This reverts commit 2490155a3fc483b3df866d19216c70fb0c1f6d58.

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -3,5 +3,6 @@ set -e
 
 uv run nuitka \
   --mode=app \
+  --linux-create-installer \
   --linux-app-icon=assets/wordee-icon.png \
   src/wordee/main.py
--- src/wordee/main.py
+++ src/wordee/main.py
@@ -1,10 +1,12 @@
 # nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --company-name=Nerrader
 # nuitka-project: --product-name=WORDEE
 # nuitka-project: --output-filename=WORDEE
 # nuitka-project: --output-dir=dist
+# nuitka-project: --product-version=0.1.0
 # nuitka-project: --assume-yes-for-downloads
-# nuitka-project: --python-flag=no_docstrings
-# nuitka-project: --python-flag=no_site
+# nuitka-project: --no_docstrings
+# nuitka-project: --no_site
 
 import argparse
 import sys
