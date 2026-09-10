f466ba9f8073e249567fcd5259fbeb7b1796e579
nerrader <nerrader@proton.me>
2026-09-10 17:46:01 +0700

    ci: put .app into .app.zip

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -7,3 +7,8 @@ uv run nuitka \
   src/wordee/main.py
 
 mv dist/main.app dist/WORDEE-x86_64.app
+
+# apparently its supposed to zip apps
+ditto -c -k --sequesterRsrc --keepParent \
+  "dist/WORDEE-x86_64.app" \
+  "dist/WORDEE-x86_64.app.zip"
