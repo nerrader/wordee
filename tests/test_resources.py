80254c7277a60e8fbbd275e799359c44e38cb6ea
nerrader <nerrader@proton.me>
2026-09-12 09:31:30 +0700

    ci: add back resources test for CI

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,19 +1,12 @@
-import os
 import subprocess
 from pathlib import Path
-
-import pytest
 
 QRC = Path("src/wordee/assets/resources.qrc")
 RESOURCES = Path("src/wordee/assets/resources_rc.py")
 
 
-@pytest.mark.skipif(
-    os.getenv("CI") == "true",
-    reason="Generated RC files differ between local and CI due to random stuff idk",
-)
 def test_resources_rc_is_up_to_date(tmp_path: Path) -> None:
-    original_rc_file = RESOURCES.read_bytes()
+    original_rc_file = RESOURCES.read_text()
 
     result = subprocess.run(
         ["uv", "run", "pyside6-rcc", QRC, "-o", tmp_path / "resources_rc.py"],
@@ -21,4 +14,4 @@ def test_resources_rc_is_up_to_date(tmp_path: Path) -> None:
     )
 
     assert result.returncode == 0
-    assert original_rc_file == (tmp_path / "resources_rc.py").read_bytes()
+    assert original_rc_file == (tmp_path / "resources_rc.py").read_text()
