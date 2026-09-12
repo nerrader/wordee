4c1253f8a3f510f5e536991dffa013a9aaf07059
nerrader <nerrader@proton.me>
2026-09-12 09:18:21 +0700

    build: change windows build to be standalone .zip to reduce antivirus false positives

---
+++ assets/README-windows.txt
@@ -0,0 +1,5 @@
+PLEASE READ
+
+DO NOT move the .exe file out of this folder without also moving the main.dist/ folder with it.
+
+The .exe depends on files inside main.dist/, so they must remain together.
--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,9 +1,34 @@
 # run this from the project root
 # all the other configs are in the project root
 uv run nuitka `
-    --onefile `
+    --mode=app `
     --windows-console-mode=disable `
     --windows-icon-from-ico=assets/wordee-icon.ico `
     src/wordee/main.py
 
-Rename-Item "dist/WORDEE.exe" "WORDEE-x86_64.exe"
+# rename the .exe
+Rename-Item `
+    "dist/WORDEE.exe" `
+    "WORDEE-x86_64.exe"
+
+# remove all useless build junk
+Remove-Item "dist/main.build" -Recurse -Force
+Remove-Item "dist/main.onefile-build" -Recurse -Force
+
+Copy-Item `
+    "assets/README-windows.txt" `
+    "dist/README.md"
+
+# zip the WORDEE-x86_64
+Compress-Archive `
+    -Path "dist/*" `
+    -DestinationPath "WORDEE-x86_64.zip" `
+    -Force
+
+# remove all items in dist/
+Remove-Item -Path "dist/*" -Recurse -Force
+
+# then move the .zip there
+Move-Item `
+    "WORDEE-x86_64.zip" `
+    "dist/WORDEE-x86_64.zip"
